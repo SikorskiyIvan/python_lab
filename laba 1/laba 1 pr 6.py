78ec548cf585589938724381e9a9d6e6bb0710b1
markoherobrin <i.sikorskiy@bk.ru>
2022-09-16 10:47:04 +0300

    1

--- laba 1/laba 1 pr 6.py
+++ laba 1/laba 1 pr 6.py
@@ -3,5 +3,5 @@ a = input("Введите числа через запятую:\n").split(",")
 for i in range(0, len(a)):
     lis.append(a[i])
 tup = tuple(lis)
-print(lis,"\n",tup)
-# print(lis.__sizeof__(),"\n", tup.__sizeof__())
+print(lis," ",tup)
+print(lis.__sizeof__()," ", tup.__sizeof__())
